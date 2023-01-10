9d9fee6205f0fb90c1572be0990d0ac70176ba1b
Mizan_thinkpadX260_Ubuntu <mizanur9522@outlook.com>
2023-01-10 16:23:11 +0000

    Complete Header and Slider and Grid Design

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const grid = document.querySelector('.grid');
-let input = prompt("Enter a number");
-let number = parseInt(input);
+const boxes = document.querySelectorAll('.box');
+let input = document.querySelector('#myRange');
+// let number = parseInt(input);
 
-
-for (let i = 1; i <= number * number; i++) {
-    const div = document.createElement('div');
-    div.style.height = `${grid.clientHeight / number}px`;
-    div.style.width = `${grid.clientWidth / number}px`;
-
-    grid.appendChild(div);
+function generateGrid(number) {
+    for (let i = 1; i <= number * number; i++) {
+        const div = document.createElement('div');
+        div.classList.add("box");
+        div.style.height = `${grid.clientHeight / number}px`;
+        div.style.width = `${grid.clientWidth / number}px`;
+    
+        grid.appendChild(div);
+    }
 }
+
+console.log(input);
+
+// generateGrid(number);
+
+
+
+
