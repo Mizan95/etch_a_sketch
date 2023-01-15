a15c81ef304698a86a84b31a567714df50e6dfca
Mizan_thinkpadX260_Ubuntu <mizanur9522@outlook.com>
2023-01-15 16:12:05 +0000

    Configure slider and clear button functions

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
 const grid = document.querySelector('.grid');
-const boxes = document.querySelectorAll('.box');
-let input = document.querySelector('#myRange');
-// let number = parseInt(input);
+const slider = document.querySelector('#slider');
+const applyButton = document.querySelector('#applyButton');
+const clearButton = document.querySelector('#clearButton');
+
+let sliderValue = "";
+
+slider.onchange = function() {
+    sliderValue = this.value;
+}
+
 
 function generateGrid(number) {
-    for (let i = 1; i <= number * number; i++) {
+    for (let i = 1; i <= number ** 2; i++) {
         const div = document.createElement('div');
         div.classList.add("box");
         div.style.height = `${grid.clientHeight / number}px`;
         div.style.width = `${grid.clientWidth / number}px`;
-    
         grid.appendChild(div);
     }
 }
 
-console.log(input);
+function clearGrid(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild)
+    }
+}
 
-// generateGrid(number);
+
+
+
+
+
+applyButton.addEventListener('click', () => {
+    generateGrid(sliderValue);
+});
+
+clearButton.addEventListener('click', () => {
+    clearGrid(grid);
+})
+
 
 
 
